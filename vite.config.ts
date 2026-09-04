c70c2bd8c186e4dc6da7b9c23940826097dad1b3
alpaslanbeyoglu <47739835+alpaslanbeyoglu@users.noreply.github.com>
2026-09-05 01:00:47 +0300

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+base: '/Mim3d/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
